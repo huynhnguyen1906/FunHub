7373e3bc18ba914ac6fe48bebb6fb64b46b08e06
グエン　ドゥック　フィン <duchuynhnguyen1906@gmail.com>
2024-01-23 18:35:49 +0900

    Add normalize.css and sass dependencies

--- server/server.js
+++ server/server.js
@@ -1,20 +1,20 @@
-require("dotenv").config()
-const express = require("express") //commonjs
-const apiRoutes = require("./src/routers/web")
-const path = require("path")
-const app = express() // app express
-const port = process.env.PORT || 8081 // port => hardcode
-const hostname = process.env.HOST_NAME
+require("dotenv").config();
+const express = require("express"); //commonjs
+const apiRoutes = require("./src/routers/web");
+const path = require("path");
+const app = express(); // app express
+const port = process.env.PORT || 8081; // port => hardcode
+const hostname = process.env.HOST_NAME;
 
-app.use(express.json())
-app.use("/", apiRoutes)
+app.use(express.json());
+app.use("/", apiRoutes);
 
-app.use(express.static(path.join(__dirname, "../client/public")))
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "../client/public/index.html"))
-})
+	res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 app.listen(port, hostname, () => {
-	console.log(`App running on http://${hostname}:${port}/`)
-})
+	console.log(`App running on http://${hostname}:${port}/`);
+});
--- server/src/routers/web.js
+++ server/src/routers/web.js
@@ -1,8 +1,8 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 // Define your routes here
 router.get("/api", (req, res) => {
-	res.send("Hello from SERVER")
-})
-module.exports = router
+	res.send("Hello from SERVER");
+});
+module.exports = router;
