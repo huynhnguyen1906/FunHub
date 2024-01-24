3fabe83bd36dec1cd4217aec9710948a27a6c86b
グエン　ドゥック　フィン <duchuynhnguyen1906@gmail.com>
2024-01-24 11:58:56 +0900

    Creating landing page Complete landing page ui

--- client/src/pages/LandingPage/index.js
+++ client/src/pages/LandingPage/index.js
@@ -1,7 +1,8 @@
 import "./style.scss";
+
 function Landing() {
 	return (
-		<div className="container">
+		<div className="lContainer">
 			<div className="hero">
 				<div className="nav">
 					<ul>
@@ -58,6 +59,86 @@ function Landing() {
 					</div>
 					<div className="infoImg"></div>
 				</div>
+				<div className="exBl">
+					<div className="introImg"></div>
+					<div className="textBlock">
+						<h2>共有しよう！！</h2>
+						<p>
+							笑い声とリラックス感にあふれたSNSを作ることを目指して、皆さんに参加と共有をお願いしています。一日の中で楽しかった瞬間や、単に笑わせてくれた瞬間を共有してください。
+							<br />
+							他の人と面白いコンテンツを共有することも、自分にとって楽しさを生み出す方法の一つだと信じています。
+						</p>
+					</div>
+				</div>
+				<div className="exBl">
+					<div className="crAccountImg"></div>
+					<div className="textBlock">
+						<h2>カウント作成は簡単！！</h2>
+						<p>
+							面倒な登録手続きは一切ありません。FunHubでは、自分のメールアドレスだけで、わずか1分でアカウントを作成できます。
+							<br />
+							多くの情報は不要で、電話番号も必要ありません。これにより、あなたの情報を安全に保ちつつ、時間も節約できます。
+						</p>
+						<br />
+						<button>サインイン</button>
+					</div>
+				</div>
+			</div>
+			<div className="botCatchCopy">
+				<div className="textBlock">
+					<h2>たくさん観て、たくさん共有しよう！</h2>
+					<br />
+					<p>
+						FunHub -
+						興味津々なコンテンツが詰まった場所。一緒に参加し、活気あるコミュニティの一部となり、創造性と楽しさが絶え間なく広がる場所。
+						<br />
+						今すぐ登録して、前向きなソーシャルエクスペリエンスを体験し、楽しさを共有する冒険を始めましょう！
+					</p>
+					<br />
+					<button>サインイン</button>
+				</div>
+			</div>
+			<div className="footer">
+				<div className="ctBox">
+					<div className="contact">
+						<br />
+						<div className="contactInfo">
+							<i className="fbIcon"></i>
+							<a href="https://www.facebook.com/Huynh.me123/" target="new">
+								Đức Huỳnh
+							</a>
+						</div>
+						<div className="contactInfo">
+							<i className="insIcon"></i>
+							<a href="https://www.instagram.com/n.d_.huynh/" target="new">
+								n.d_.huynh
+							</a>
+						</div>
+						<div className="contactInfo">
+							<i className="gitHubIcon"></i>
+							<a href="https://github.com/huynhnguyen1906" target="new">
+								huynhnguyen1906
+							</a>
+						</div>
+					</div>
+					<div className="footerLogo"></div>
+					<div className="footerNav">
+						<br />
+						<ul>
+							<li>Top</li>
+							<li>About</li>
+							<li>Contact</li>
+						</ul>
+					</div>
+				</div>
+				<p className="cpRight">
+					<small>
+						&copy;Create by{" "}
+						<a href="https://github.com/huynhnguyen1906" target="new">
+							Huynhnguyen1906
+						</a>
+					</small>
+				</p>
 			</div>
 		</div>
 	);
