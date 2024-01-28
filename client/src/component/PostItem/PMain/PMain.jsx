1e511b971375771d7b88963003e66c22316a44fd
huynhnguyen1906 <duchuynhnguyen1906@gmail.com>
2024-01-28 16:28:56 +0900

    Creating postItem....

---
+++ client/src/component/PostItem/PMain/PMain.jsx
@@ -0,0 +1,12 @@
+function PMain() {
+	return (
+		<div className="PMain">
+			<img
+				src="https://scontent-itm1-1.xx.fbcdn.net/v/t39.30808-6/423161608_759792852688906_6467791457022407579_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=3635dc&_nc_ohc=-nPpK-1_LhcAX-q1b_n&_nc_ht=scontent-itm1-1.xx&oh=00_AfDlQqbj2WLObJfYBRJ7ei5OxwubHTRr2zf5TcL_gMPxEg&oe=65BB2F60"
+				alt=""
+			/>
+		</div>
+	);
+}
+
+export default PMain;
--- client/src/component/PostItem/PostItem.jsx
+++ client/src/component/PostItem/PostItem.jsx
@@ -1,9 +1,42 @@
 import "./PostItem.scss";
+import PMain from "./PMain/PMain";
 function PostItem() {
 	return (
 		<div className="PostItem">
 			<div className="pItemTop">
-				<div className="userInfo"></div>
+				<div className="userInfoW">
+					<div className="userInfo">
+						<div className="userInfoK">
+							<div className="userIcon">
+								<img src="" alt="" />
+							</div>
+							<div className="userName">QTaro</div>
+						</div>
+						<div className="postTime">ｘ時間前</div>
+					</div>
+					<div className="btn">
+						<span></span>
+					</div>
+				</div>
+				<div className="PTextW">
+					<div className="PText">
+						It is a long established fact that a reader will be distracted by
+						the readable content of a page when looking at its layout. The point
+						of using Lorem Ipsum is that it has a more-or-less normal
+						distribution of letters, as opposed to using 'Content here, content
+						here', making it look like readable English.
+					</div>
+				</div>
+			</div>
+			<div className="PMainW">
+				<PMain />
+			</div>
+			<div className="pItemBottom">
+				<div className="btnBox">
+					<div className="btn"></div>
+					<div className="btn"></div>
+					<div className="btn"></div>
+				</div>
 			</div>
 		</div>
 	);
