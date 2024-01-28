70c6f66bfbe8ebd4544626912b457664ec1dd2c3
huynhnguyen1906 <duchuynhnguyen1906@gmail.com>
2024-01-29 03:04:47 +0900

    Created postItem component Complete UI render logic

--- client/src/component/PostItem/PMain/PMain.jsx
+++ client/src/component/PostItem/PMain/PMain.jsx
@@ -1,10 +1,18 @@
-function PMain() {
+function PMain({ media, post }) {
 	return (
 		<div className="PMain">
-			<img
-				src="https://scontent-itm1-1.xx.fbcdn.net/v/t39.30808-6/423161608_759792852688906_6467791457022407579_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=3635dc&_nc_ohc=-nPpK-1_LhcAX-q1b_n&_nc_ht=scontent-itm1-1.xx&oh=00_AfDlQqbj2WLObJfYBRJ7ei5OxwubHTRr2zf5TcL_gMPxEg&oe=65BB2F60"
-				alt=""
-			/>
+			{media.type === "video" ? (
+				<>
+					<video controls={post.media.length === 1} src={media.url} />
+					{post.media.length > 2 && (
+						<div className="videoW">
+							<span></span>
+						</div>
+					)}
+				</>
+			) : (
+				<img src={media.url} alt="" />
+			)}
 		</div>
 	);
 }
--- client/src/component/PostItem/PostItem.jsx
+++ client/src/component/PostItem/PostItem.jsx
@@ -1,6 +1,61 @@
 import "./PostItem.scss";
+import { useState } from "react";
 import PMain from "./PMain/PMain";
-function PostItem() {
+
+const postTimeFormat = (time) => {
+	const notiTime = new Date(time);
+	const now = new Date();
+	const nowUTC = new Date(now.toUTCString());
+	const diff = nowUTC - notiTime;
+	const diffMin = Math.floor(diff / 60000);
+	const diffHour = Math.floor(diffMin / 60);
+	const diffDay = Math.floor(diffHour / 24);
+	const diffMonth = Math.floor(diffDay / 30);
+	const diffYear = Math.floor(diffMonth / 12);
+	if (diffYear > 0) {
+		return `${diffYear}年前`;
+	} else if (diffMonth > 0) {
+		return `${diffMonth}ヶ月前`;
+	} else if (diffDay > 0) {
+		return `${diffDay}日前`;
+	} else if (diffHour > 0) {
+		return `${diffHour}時間前`;
+	} else if (diffMin > 0) {
+		return `${diffMin}分前`;
+	} else {
+		return `たった今`;
+	}
+};
+
+const CountFormat = (count) => {
+	if (count > 10000) {
+		return `${Math.floor(count / 10000)}万${Math.floor(count % 10000)}`;
+	} else {
+		return `${count}`;
+	}
+};
+
+function PostItem({ post }) {
+	const postDate = new Date(post.timestamp);
+	const [isExpanded, setIsExpanded] = useState(false);
+
+	const toggleExpanded = () => {
+		setIsExpanded(!isExpanded);
+	};
+	let PMainClass = "PMainW";
+	if (post.media.length === 2) {
+		PMainClass += " PMainWTwo";
+	} else if (post.media.length === 3) {
+		PMainClass += " PMainWThree";
+	} else if (post.media.length >= 4) {
+		PMainClass += " PMainWFour";
+	}
+
+	let seeMoreClass = "seeMore none";
+	if (post.media.length > 4) {
+		seeMoreClass = " seeMore";
+	}
+
 	return (
 		<div className="PostItem">
 			<div className="pItemTop">
@@ -8,11 +63,11 @@ function PostItem() {
 					<div className="userInfo">
 						<div className="userInfoK">
 							<div className="userIcon">
-								<img src="" alt="" />
+								<img src={post.user.avatar} alt="" />
 							</div>
-							<div className="userName">QTaro</div>
+							<div className="userName">{post.user.name}</div>
 						</div>
-						<div className="postTime">ｘ時間前</div>
+						<div className="postTime">{postTimeFormat(postDate)}</div>
 					</div>
 					<div className="btn">
 						<span></span>
@@ -20,22 +75,51 @@ function PostItem() {
 				</div>
 				<div className="PTextW">
 					<div className="PText">
-						It is a long established fact that a reader will be distracted by
-						the readable content of a page when looking at its layout. The point
-						of using Lorem Ipsum is that it has a more-or-less normal
-						distribution of letters, as opposed to using 'Content here, content
-						here', making it look like readable English.
+						{post.content.length > 400 && !isExpanded
+							? `${post.content.substring(0, 400)}...`
+							: post.content}
+						{post.content.length > 400 && (
+							<span onClick={toggleExpanded}>
+								{isExpanded ? "" : "  すべてを見る"}
+							</span>
+						)}
 					</div>
 				</div>
 			</div>
-			<div className="PMainW">
-				<PMain />
+			<div className={PMainClass}>
+				{post.media.slice(0, 4).map((mediaItem, index) => (
+					<PMain key={index} media={mediaItem} post={post} />
+				))}
+				<div className={seeMoreClass}>{`${post.media.length % 4} 件以上`}</div>
 			</div>
 			<div className="pItemBottom">
+				<div className="reactCount">
+					<div className="likeCount">
+						<i></i>
+						{CountFormat(post.likes)}
+					</div>
+					<div className="cmtCount">コメント{CountFormat(post.comments)}件</div>
+				</div>
+				<div className="bar"></div>
 				<div className="btnBox">
-					<div className="btn"></div>
-					<div className="btn"></div>
-					<div className="btn"></div>
+					<div className="btnW">
+						<div className="btn active">
+							<i></i>
+							いいね！
+						</div>
+					</div>
+					<div className="btnW">
+						<div className="btn">
+							<i></i>
+							コメントする
+						</div>
+					</div>
+					<div className="btnW">
+						<div className="btn">
+							<i></i>
+							シェア
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
