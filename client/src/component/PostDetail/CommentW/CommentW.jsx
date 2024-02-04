db5f695ceb7282b326cd51287311619445baa253
huynhnguyen1906 <duchuynhnguyen1906@gmail.com>
2024-02-05 04:58:38 +0900

    Add time formatting function to CommentW component

--- client/src/component/PostDetail/CommentW/CommentW.jsx
+++ client/src/component/PostDetail/CommentW/CommentW.jsx
@@ -1,5 +1,30 @@
 import { useState } from "react";
+import moment from "moment-timezone";
 
+const timeFormat = (time) => {
+	const notiTime = moment.utc(time).add(9, "hours").toDate();
+	const now = new Date();
+	const nowUTC = new Date(now.toUTCString());
+	const diff = nowUTC - notiTime;
+	const diffMin = Math.floor(diff / 60000);
+	const diffHour = Math.floor(diffMin / 60);
+	const diffDay = Math.floor(diffHour / 24);
+	const diffMonth = Math.floor(diffDay / 30);
+	const diffYear = Math.floor(diffMonth / 12);
+	if (diffYear > 0) {
+		return `${diffYear}年前`;
+	} else if (diffMonth > 0) {
+		return `${diffMonth}ヶ月前`;
+	} else if (diffDay > 0) {
+		return `${diffDay}日前`;
+	} else if (diffHour > 0) {
+		return `${diffHour}時間前`;
+	} else if (diffMin > 0) {
+		return `${diffMin}分前`;
+	} else {
+		return `たった今`;
+	}
+};
 function CommentW({ comment }) {
 	const [CIsExpanded, CSetIsExpanded] = useState(false);
 	const toggleExpanded = () => {
@@ -14,7 +39,7 @@ function CommentW({ comment }) {
 				<div className="cmtContent">
 					<div className="cmtInfo">
 						<div className="userName">{comment.user.fullName}</div>
-						<div className="cmtTime">2年前</div>
+						<div className="cmtTime">{timeFormat(comment.create_at)}</div>
 					</div>
 					<div className={`cmtText ${comment.content === "" ? "none" : ""}`}>
 						{comment.content && comment.content.length > 175 && !CIsExpanded
