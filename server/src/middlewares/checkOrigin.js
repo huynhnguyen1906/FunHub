b309534bc9b5637b4868e8db1e2ac70222b464a0
NGUYEN DUC HUYNH <duchuynhnguyen1906@gmail.com>
2024-10-21 14:32:26 +0900

    Enhance origin check middleware to validate referer header only for API routes

--- server/src/middlewares/checkOrigin.js
+++ server/src/middlewares/checkOrigin.js
@@ -1,7 +1,8 @@
 const path = require("path");
 
 function checkOrigin(req, res, next) {
-	if (!req.headers.referer) {
+	// Kiểm tra nếu không có header Referer và đường dẫn bắt đầu bằng /api
+	if (!req.headers.referer && req.path.startsWith("/api")) {
 		return res.status(403).sendFile(path.join(__dirname, "../public/index.html"));
 	}
 	next();
